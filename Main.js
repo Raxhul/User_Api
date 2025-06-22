899837a43503de7194194953ebbe878a783939e5
raxhul <gv807962@gmail.com>
2025-06-22 13:11:27 +0530

    Enabled

--- Main.js
+++ Main.js
@@ -1,16 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
+
 import UserRoute  from './routes/user_route.js'
 import Database from './Database/Database.js'
+import cors from "cors";
+
 
 // save it in a variable
 const app = express();
-const cors = require("cors");
+
+//
+app.use(cors());
+
+
 // port Number to listen
 const port = 2810;
 
 //the middle ware 
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
